74bd34e7fd49deae009fe57986e4ee60a48cd956
daniel-qwerty <daniel@qwerty.com.bo>
2016-11-07 16:05:39 -0400

    asd

--- Modules/Pages/Widget/SocialIcons.php
+++ Modules/Pages/Widget/SocialIcons.php
@@ -19,9 +19,9 @@ class Pages_Widget_SocialIcons extends Com_Object {
     public function render() {
         ?>
         <ul class="unstyled inline social-icons social-simple">
-            <li> <a target="_blank" href="<?PHP echo Links_Helper_Link::getInstance()->get('LinkFacebook')->LinUrl; ?>"><img src="<?PHP echo Com_Helper_Url::getInstance()->getImage(); ?>/Public/hotel/fb.png"></a> </li>
-            <li> <a target="_blank" href="<?PHP echo Links_Helper_Link::getInstance()->get('LinkInstagram')->LinUrl; ?>"><img src="<?PHP echo Com_Helper_Url::getInstance()->getImage(); ?>/Public/hotel/instagram.png"></a> </li>
-            <li> <a target="_blank" href="<?PHP echo Links_Helper_Link::getInstance()->get('LinkTripadvisor')->LinUrl; ?>"><img src="<?PHP echo Com_Helper_Url::getInstance()->getImage(); ?>/Public/hotel/tripadvisor.png"></a> </li>
+            <li> <a target="_blank" href="<?PHP echo Links_Helper_Link::getInstance()->get('LinkFacebook')->LinUrl; ?>"><img src="<?PHP echo Com_Helper_Url::getInstance()->getImage(); ?>/Public/hotel/es.png"></a> </li>
+            <li> <a target="_blank" href="<?PHP echo Links_Helper_Link::getInstance()->get('LinkInstagram')->LinUrl; ?>"><img src="<?PHP echo Com_Helper_Url::getInstance()->getImage(); ?>/Public/hotel/en.png"></a> </li>
+            <li> <a target="_blank" href="<?PHP echo Links_Helper_Link::getInstance()->get('LinkTripadvisor')->LinUrl; ?>"><img src="<?PHP echo Com_Helper_Url::getInstance()->getImage(); ?>/Public/hotel/pt.png"></a> </li>
         </ul>
         <?PHP
     }
